ab255d9e55161c350239ea26b52bedb4f06f237c
krishnapriya-21 <you@example.com>
2025-09-05 12:31:48 +0530

    Vite Deploy Edits

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import {defineConfig} from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
+	base: '/React-Travel-Journal-WebPage/', // Use your repo name here
 	plugins: [
 		react()
 	]
